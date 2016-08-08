81524fb3defe9d624e55aa82a77aa2cdd2e5e2f1
Derek Hoang <hoangda23@gmail.com>
2016-08-08 12:30:47 -0700

    edited server

--- server/aws.js
+++ server/aws.js
@@ -8,3 +8,28 @@ AWS.config.update({region: 'us-west-2'});
 //  endpoint to connect to postgres db nmodb.cqdxw6kmfwjk.us-west-2.rds.amazonaws.com:5432
 
 // var ec2 = new AWS.EC2({apiVersion: 'latest'});
+var ec2 = new AWS.EC2();
+
+var params = {
+  ImageId: 'ami-1624987f', // Amazon Linux AMI x86_64 EBS
+  InstanceType: 't1.micro',
+  MinCount: 1, MaxCount: 1
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { console.log("Could not create instance", err); return; }
+
+  var instanceId = data.Instances[0].InstanceId;
+  console.log("Created instance", instanceId);
+
+  // Add tags to the instance
+  params = {Resources: [instanceId], Tags: [
+    {Key: 'Name', Value: 'instanceName'}
+  ]};
+  ec2.createTags(params, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+});
+
+// [1] 32189
--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(3000, () => {
-  console.log('listening to port 3000')
+app.get('/'., function(req, res) {
+  res.sendFile(path.join(__dirname, '/client/src/index.html'));
+})
+
+app.listen(9000, () => {
+  console.log('listening to port 9000')
 })
