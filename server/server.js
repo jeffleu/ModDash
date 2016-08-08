ba311ed1dc75cfac1c24ccd164662d48c2c3e14b
Derek Hoang <hoangda23@gmail.com>
2016-08-08 13:00:06 -0700

    server serving html file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(path.join(__dirname, '../client/src')));
 
-app.get('/'., function(req, res) {
-  res.sendFile(path.join(__dirname, '/client/src/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/../client/src/index.html'));
 })
 
 app.listen(9000, () => {
