f053ac5876f978f9175b2e07f4edba457578836d
Derek Hoang <hoangda23@gmail.com>
2016-08-09 13:59:37 -0700

    notifications completed

--- server/db/sequelize.js
+++ server/db/sequelize.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 //TRY sequelize above first, if it doesn't work try the one below
 
-const sequelize = new Sequelize('nmodb', process.env.PSQL_username, process.emv.password, {
+const sequelize = new Sequelize('nevermissout', process.env.PSQL_username, process.emv.password, {
   dialect: 'postgres',
   protocol: 'postgres',
   port: 5432,
-  host: 'ec2-52-33-110-254.us-west-2.compute.amazonaws.com',
+  host: 'nevermissout.cqdxw6kmfwjk.us-west-2.rds.amazonaws.com',
   dialectOptions: {
     ssl: true,
   },
