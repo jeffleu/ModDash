de3b649cfe6b270096c50a38ef81f8ca8fa107e4
Derek Hoang <hoangda23@gmail.com>
2016-08-09 15:10:11 -0700

    added sample dummy event post data

--- client/src/chrome/background.js
+++ client/src/chrome/background.js
@@ -3,6 +3,21 @@ let newEvents = [];
 let ids = [];
 let latestId;
 
+var event = {
+    'summary': 'Suyash Kale Birthday', // here comes the title of you event;
+    'location': 'Sai Kripa Colony, Scheme No 171, Indore, Madhya Pradesh', // location, you can find the exact location here;
+    'description': 'A chance to hear more about Google's developer products.',
+    'start': { // here comes the event's start time details;
+        'dateTime': '2015-05-28T09:00:00-07:00', // date and time for the event;
+        'timeZone': 'America/Los_Angeles' // time zone for the event;
+    },
+    'end': { // here comes the event's end time details;
+        'dateTime': '2015-05-28T17:00:00-07:00', // date and time for the event;
+        'timeZone': 'America/Los_Angeles' // time zone for the event;
+    }
+  };
+
+
 
 function fetchData(data) {
   for (var i = 0; i < data.items.length; i++) {
