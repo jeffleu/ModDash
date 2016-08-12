cdb2ceb14e7b6150114f1796543470ab6202819f
Derek Hoang <hoangda23@gmail.com>
2016-08-11 21:40:59 -0700

    add calendar component

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import FullCalendar from 'rc-calendar';
+
+
+
+class Calendar extends React.Component {
+
+  render() {
+    return(
+      <div style={{ zIndex: 1000, position: 'relative' }}>
+
+      </div>
+    )
+  }
+}
+
+export default Calendar;
