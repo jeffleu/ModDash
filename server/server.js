2a24554756fecff77b0a2b7389cb31d7acafcef8
Jeff Leu <babybluejeff@Jeffs-MacBook-Pro.local>
2016-08-12 11:32:12 -0700

    Resolved merge conflicts and removed old style.css.

--- server/server.js
+++ server/server.js
@@ -22,10 +22,9 @@ app.all('/api/*', function(req, res, next) {
 })
 app.use('/api', router);
 
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname + '/../client/src/index.html'));
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/../client/public/dist/index.html'));
 });
-
 
 server.listen(9000, () => {
   console.log('listening to port 9000');
