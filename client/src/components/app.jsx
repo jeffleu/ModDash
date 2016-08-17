c22679317a9768fbb7224929bb173129eb0adc94
Jeff Leu <babybluejeff@Jeffs-MacBook-Pro.local>
2016-08-16 17:04:10 -0700

    Removed geolocation code out of App component.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -17,59 +17,6 @@ class App extends React.Component {
     }
   }
 
-  // getGeolocation() {
-  //   let geoOptions = { timeout: 10000 };
-    
-  //   const geoSuccess = (position) => {
-  //     let geolocation = `${position.coords.latitude} ${position.coords.longitude}`;
-
-  //     fetch('http://localhost:9000/api/users/getGeolocation')
-  //       .then((res) => res.json())
-  //         .then((data) => {
-  //           // If current geolocation doesn't match geolocation in DB, then update DB
-  //           if (data.geolocation !== geolocation) {
-  //             this.updateGeolocation(geolocation);
-  //           } else {
-  //             console.log('Geolocation not updated as current geolocation has not changed.');
-  //           }
-  //         })
-  //         .catch((err) => {
-  //           console.log('Error retrieving user\'s geolocation from database.\n', err);
-  //         });
-  //   };
-
-  //   const geoError = (error) => {
-  //     if (error.code === 0) {
-  //       console.log(`Geolocation error occurred: unknown error`);
-  //     } else if (error.code === 1) {
-  //       console.log(`Geolocation error occurred: permission denied`);
-  //     } else if (error.code === 2) {
-  //       console.log(`Geolocation error occurred: position unavailable`);
-  //     } else if (error.code === 3) {
-  //       console.log(`Geolocation error occurred: timed out`);
-  //     }
-  //   };
-
-  //   navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-  // }
-
-  // updateGeolocation(geolocation) {
-  //   fetch('http://localhost:9000/api/users/updateGeolocation', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({ geolocation })
-  //   })
-  //   .then((success) => {
-  //     console.log('Geolocation was successfully updated!');
-  //   })
-  //   .catch((err) => {
-  //     console.log('An error occurred while updating geolocation.');
-  //   });
-  // }
-
   fetchAndUpdateEvents() {
     fetch('http://localhost:9000/api/calendar/getEvent')
       .then((res) => res.json())
@@ -114,12 +61,6 @@ class App extends React.Component {
 
   componentDidMount() {
     this.fetchAndUpdateEvents();
-    // this.getGeolocation();
-
-    // // Checks for user's geolocation every 10 minutes
-    // setInterval(() => {
-    //   this.getGeolocation();
-    // }, 600000);
   }
 
   render() {
