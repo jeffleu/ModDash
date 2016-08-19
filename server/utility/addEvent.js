a0c91478e39f1c1126ecc204934b800d9692c432
Carl Chen <carlchen214@gmail.com>
2016-08-19 16:52:54 -0700

    refactor googleCal utility functions out into its own file

--- server/utility/addEvent.js
+++ server/utility/addEvent.js
@@ -1,30 +1,18 @@
 const EventController = require('../db/controllers').EventController;
 // const { EventController } = require('../db/controllers');
 const googleOAuth = require('../setup/googleOAuth.js');
-// const google = require('googleapis');
-// const calendar = google.calendar('v3');
-// var oauth2Client = googleOAuth.oauth2Client;
 const pubnub = require('../setup/pubnub.js')
 const googleCal = require('./calendar/googleCal')
-// const Promise = require('bluebird');
 
-// calendar.events.insert = Promise.promisify(calendar.events.insert);
 // function(userId, eventDetails);
 const addEvent = function(req, res) {
-  // lines 15?-18 should be refactored into a calendar function
   return googleOAuth.getUserTokens(req.userId)
   .then((oauth2Client) => {
-    return googleCal.insertEvent(oauth2Client, req.body);
-    // .then(data => {
-    //   return data;
-    // });
-    // const params = {calendarId: 'primary', auth: oauth2Client, resource: req.body}; 
-    // .insert(params)
-    // turn this into a google calendar function module
+    return googleCal.insertEvent(oauth2Client, req.body)
   })
   .then(data => {
     // res.send should be in the parent composition function
-    // res.send(data);
+    res.sendStatus(200);
     pubnub.publishEventAdded(req.userId, data); 
     return EventController.insertEvent(req.userId, data);     
     // make everything look like this insertEvent function
--- server/utility/calendar/googleCal.js
+++ server/utility/calendar/googleCal.js
@@ -1,8 +1,12 @@
 const Promise = require('bluebird');
 const google = require('googleapis');
 var calendar = google.calendar('v3');
+const googleOAuth = require('../../setup/googleOAuth');
+const EventController = require('../../db/controllers').EventController;
+// const { UserController, EventController } = require('../db/controllers');
 
-calendar.events = Promise.promisifyAll(calendar.events);
+calendar.events.insert = Promise.promisify(calendar.events.insert);
+calendar.events.list = Promise.promisify(calendar.events.list);
 
 const insertEvent = (auth, resource) => {
   let params = {
@@ -11,10 +15,28 @@ const insertEvent = (auth, resource) => {
     resource
   };
   return calendar.events.insert(params)
-  .then(data => data);
-  // .then(data => data);
 }
 
+const getAllEventsFromCalendar = (req, res) => {
+  return googleOAuth.getUserTokens(req.userId)
+  .then(data => {
+    calendar.events.list({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      singleEvents: true,
+      minTime: Date.now()
+      // not sure about the params to get all events or get new events that we don't have yet.
+    })
+    .then(data => {
+      data.items.forEach(event => {
+        Event.insertEvent(event, userId);
+      });
+      res.send(data.items);      
+    });
+  });
+};
+
 module.exports = {
-  insertEvent
+  insertEvent,
+  getAllEventsFromCalendar
 };
