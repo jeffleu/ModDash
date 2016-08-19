6722eaa17a8a89032fa13fcfc406ace340eddb98
Carl Chen <carlchen214@gmail.com>
2016-08-19 11:52:25 -0700

    refactor google extension OAuth and web calendar OAuth into one googleOAuth file

--- server/setup/googleOAuth.js
+++ server/setup/googleOAuth.js
@@ -1,21 +1,51 @@
 const google = require('googleapis');
-const calendar = google.calendar('v3');
 const plus = google.plus('v1');
-
-
+const Promise = require('bluebird');
+plus.people.get = Promise.promisify(plus.people.get);
 var OAuth2 = google.auth.OAuth2;
 
 var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, 
   process.env.GOOGLE_CLIENT_SECRET, 
   'http://localhost:9000/verified');
 
-// generate consent page url
+oauth2Client.getToken = Promise.promisify(oauth2Client.getToken);
+
+// generate consent page url with our required access scopes
 var url = oauth2Client.generateAuthUrl({
-  access_type: 'offline', // will return a refresh token
-  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read', 'https://www.googleapis.com/auth/calendar'] // can be a space-delimited string or an array of scopes
+  access_type: 'offline',
+  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read', 'https://www.googleapis.com/auth/calendar'] 
 });
+
+const googleCalAuthCallback = (code) => {
+  // not sure how to fix this promise chain because I need to return tokens and profile for the next function. 
+  // plus.people.get can only return profile and may not be able return both tokens and profile at the same time until people.get promise returns
+  return oauth2Client.getToken(code)
+  .then(tokens => {
+    oauth2Client.setCredentials(tokens);
+    return plus.people.get({ userId: 'me', auth: oauth2Client })
+    .then(profile => {
+      var user = {
+        tokens: tokens,
+        profile: profile
+      }
+      return user;
+    })
+  })
+  .catch(err => {
+    console.warn('error in getting web oAuth token', err);  
+  })
+}
+
+const extensionIdentityAuth = (token) => {
+  oauth2Client.setCredentials({
+    access_token: token  
+  });
+  return plus.people.get({ userId: 'me', auth: oauth2Client })
+}
 
 module.exports = {
   oauth2Client,
-  url
+  url,
+  googleCalAuthCallback,
+  extensionIdentityAuth
 }
