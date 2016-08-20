d36df60cba479a0c80404171ca199372a7010754
Jeff Leu <babybluejeff@Jeffs-MacBook-Pro.local>
2016-08-19 18:57:15 -0700

    Commit before pulling master repo with Derek's changes.

--- server/utility/calendar/googleCal.js
+++ server/utility/calendar/googleCal.js
@@ -9,13 +9,13 @@ calendar.events.insert = Promise.promisify(calendar.events.insert);
 calendar.events.list = Promise.promisify(calendar.events.list);
 
 const insertEvent = (auth, resource) => {
-  let params = {
-    calendarId: 'primary',
+  var params = {
+    calendarId: 'primary', 
     auth,
     resource
   };
-  return calendar.events.insert(params)
-}
+  return calendar.events.insert(params);
+};
 
 const getAllEventsFromCalendar = (req, res) => {
   return googleOAuth.getUserTokens(req.userId)
