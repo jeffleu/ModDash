f9870f8717ee243218f7c236d7ca1757f9d5919b
Jeff Leu <babybluejeff@Jeffs-MacBook-Pro.local>
2016-08-19 19:49:25 -0700

    Replaced the 'let' with 'var' as it was causing an error on the server.

--- server/utility/calendar/googleCal.js
+++ server/utility/calendar/googleCal.js
@@ -11,10 +11,30 @@ const insertEvent = (auth, resource) => {
     auth,
     resource
   };
-  
+
   return calendar.events.insert(params);
 };
 
+const getAllEventsFromCalendar = (req, res) => {
+  return googleOAuth.getUserTokens(req.userId)
+  .then(data => {
+    calendar.events.list({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      singleEvents: true,
+      minTime: Date.now()
+      // not sure about the params to get all events or get new events that we don't have yet.
+    })
+    .then(data => {
+      data.items.forEach(event => {
+        Event.insertEvent(event, userId);
+      });
+      res.send(data.items);      
+    });
+  });
+};
+
 module.exports = {
-  insertEvent
+  insertEvent,
+  getAllEventsFromCalendar
 };
