f3b5c3017851ce3ea56bac87e17c5d34337d9034
Carl Chen <carlchen214@gmail.com>
2016-08-19 21:03:32 -0700

    reorganize file architecture on client side

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   entry: [path.resolve(__dirname, 'client', 'src', 'components', 'App.jsx'), 'whatwg-fetch'],
   output: {
-    path: __dirname + '/client/public/dist',
+    path: __dirname + '/client/dist',
     filename: 'bundle.js'
   },
   devServer: {
     inline: true,
-    contentBase: './client/public/dist',
+    contentBase: __dirname + '/client/dist',
     port: 8100
   },
   module: {
