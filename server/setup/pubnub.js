30c6779f4da02a49de4410a1e878b27ef7b3cbbb
Carl Chen <carlchen214@gmail.com>
2016-08-22 14:35:48 -0700

    refactor pubnub publish functions into /server/utility/notification

--- server/setup/pubnub.js
+++ server/setup/pubnub.js
@@ -1,4 +1,3 @@
-const User = require('../db/queries').User;
 const PubNub = require('pubnub');
 
 var pubnub = new PubNub({
@@ -12,60 +11,4 @@ var pubnub = new PubNub({
     heartbeatInterval: 60
 });
 
-const publishEventAdded = (userId, message) => {
-  message.messageType = 'eventAdded';
-  User.getUser(userId)
-  .then(user => {
-    var channel = user.dataValues.pubnubid;
-    pubnub.publish({
-        message,
-        channel,
-        sendByPost: false, // true to send via post
-        storeInHistory: false, // override default storage options
-        meta: {} // publish extra meta with the request
-      },
-      (status, response) => {
-        // handle status, response
-        console.log('pubnub notification "eventAdded" was sent to client');
-      }
-    );
-  })
-  .catch(err => {
-    console.warn('did not find user in pubnub publishEventAdded');
-  });
-};
-
-const publishTimeToLeave = (channel, message) => {
-  message.messageType = 'timeToLeave';
-  pubnub.publish({
-    message,
-    channel,
-    sendByPost: false, // true to send via post
-    storeInHistory: false // override default storage options
-    // meta: { "cool": "meta" } // publish extra meta with the request
-    },
-    (status, response) => {
-      // Handle status and response
-      console.log('Map notification was created.')
-    }
-  );
-};
-
-// example of publishing a message
-// pubnub.publish(
-//   {
-//     message: { such: 'object' },
-//     channel: 'ch1',
-//     sendByPost: false, // true to send via post
-//     storeInHistory: false, //override default storage options
-//     meta: { "cool": "meta" } // publish extra meta with the request
-//   },
-//   function (status, response) {
-//       // handle status, response
-//   }
-// ); 
-
-module.exports = {
-  publishEventAdded,
-  publishTimeToLeave
-};
+module.exports = pubnub; 
