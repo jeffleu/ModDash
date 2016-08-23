ef5e39060294a9c712f2cdc25ddbbae2632f7dd5
Carl Chen <carlchen214@gmail.com>
2016-08-23 14:21:28 -0700

    create web folder and file structure for splash page

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,58 @@
 const path = require('path');
 
-module.exports = {
-  entry: [path.resolve(__dirname, 'client', 'src', 'components', 'App.jsx'), 'whatwg-fetch'],
-  output: {
-    path: __dirname + '/client/dist',
-    filename: 'bundle.js'
+module.exports = [
+  {
+    name: "extension",
+    entry: [path.resolve(__dirname, 'client', 'src', 'components', 'App.jsx'), 'whatwg-fetch'],
+    output: {
+      path: __dirname + '/client/dist',
+      filename: 'bundle.js'
+    },
+    // devServer: {
+    //   inline: true,
+    //   contentBase: __dirname + '/client/dist',
+    //   port: 8100
+    // },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          include: /client/,
+          loaders: ['react-hot', 'babel']
+        }
+      ],
+    },
+    externals: {
+      "jquery": "jQuery",
+      "Chrono": "Chrono",
+      "artyom": "artyom",
+      "moment": "moment"
+    }    
   },
-  devServer: {
-    inline: true,
-    contentBase: __dirname + '/client/dist',
-    port: 8100
-  },
-  module: {
-    loaders: [
+  {
+    name: "web",
+    entry: __dirname + '/web/index.jsx',
+    output: {
+      path: __dirname + '/web/dist',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      inline: true,
+      contentBase: __dirname+'/web/dist',
+      port: 8100
+    },
+    module: {
+      loaders: [
       {
         test: /\.jsx?/,
         exclude: /node_modules/,
-        include: /client/,
+        include: /web/,
         loaders: ['react-hot', 'babel']
-      }
-    ],
-  },
-  externals: {
-    "jquery": "jQuery",
-    "Chrono": "Chrono",
-    "artyom": "artyom",
-    "moment": "moment"
+      }]
+    }
+
   }
-};
+];
+
+
