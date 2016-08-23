b56c07383d8d078ec28be6f5870651c5df3e223a
Derek Hoang <hoangda23@gmail.com>
2016-08-23 10:18:00 -0700

    changed local host to ec2 instance

--- server/setup/googleOAuth2.js
+++ server/setup/googleOAuth2.js
@@ -1,12 +1,12 @@
 const google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
-var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'http://localhost:9000/verified');
+var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'ec2-52-33-110-254.us-west-2.compute.amazonaws.com:443/verified');
 
 // generate consent page url with our required access scopes
 var url = oauth2Client.generateAuthUrl({
   access_type: 'offline',
-  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read', 'https://www.googleapis.com/auth/calendar'] 
+  scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read', 'https://www.googleapis.com/auth/calendar']
 });
 
 module.exports = {
