48d045b3943631bc8b4bdbe7d210ccf2a168c023
Derek Hoang <hoangda23@gmail.com>
2016-08-23 11:41:33 -0700

    change port 80 to 9000 for testing ec2

--- server/setup/googleOAuth2.js
+++ server/setup/googleOAuth2.js
@@ -1,7 +1,7 @@
 const google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
-var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'ec2-52-33-110-254.us-west-2.compute.amazonaws.com:80/verified');
+var oauth2Client = new OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, 'ec2-52-33-110-254.us-west-2.compute.amazonaws.com:9000/verified');
 
 // generate consent page url with our required access scopes
 var url = oauth2Client.generateAuthUrl({
