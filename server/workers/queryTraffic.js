90613e6c691044e114a422d0337b3b6ead02ffb6
Carl Chen <carlchen214@gmail.com>
2016-08-24 12:18:25 -0700

    fix removing jobs, add note in queryTraffic about how query times are messed up, needs fix

--- server/workers/queryTraffic.js
+++ server/workers/queryTraffic.js
@@ -5,10 +5,11 @@ const sendLeaveNotification = require ('./sendLeaveNotification');
 
 const recurringCheck = (event) => {
   console.log('inside recurringCheck');  
-  const timeToCheckAgain = 180000; // 3 min
+  const timeToCheckAgain = 180000; // check every 3 min
   getTrafficTime(event)
   .then(times => {
-    if (Date.now() + times.traffic + 300000 >= Date.parse(event.dataValues.startdatetime)) {
+    // 15 minutes before hand
+    if (Date.now() + times.traffic + 1500000 >= Date.parse(event.dataValues.startdatetime)) {
       console.log('traffic is over start time, sending notification'); 
       event.dataValues.traffic = times.traffic;
       sendLeaveNotification(times.notificationTime, event.dataValues);
@@ -28,13 +29,13 @@ agenda.define('query traffic', (job, done) => {
   var travel = job.attrs.data;
   var eventId = travel.eventId;
 
-  return Event.retrieveEvent(eventId)
+  Event.retrieveEvent(eventId)
   .then(event => {
     recurringCheck(event);
   });
 
   setTimeout(() => {
-    removeJob(job.attrs._id);
+    removeQueryTraffic(job.attrs._id);
   }, 1000);
     
   // then agenda.cancel({id: jobiD}) or some other kind of identifier of the job we were doing
@@ -44,22 +45,25 @@ agenda.define('query traffic', (job, done) => {
 // function for scheduling the job
 const queryTraffic = travel => {
   travel = travel.dataValues;
-  console.log('scheduling a queryTraffic worker for destination', travel.destination,'to begin querying at', travel.queryTime)
-  if (travel.queryTime <= Date.now()) {
-    // if queryTime is in the past, query now, or maybe don't query at all?
-    agenda.now('query traffic', travel);
-  } else {
+  if (travel.queryTime >= Date.now()) {
+    console.log('scheduling a queryTraffic worker for destination', travel.destination,'to begin querying at', travel.queryTime)
     agenda.schedule(travel.queryTime, 'query traffic', travel);
+  } else {
+    console.log('query time is in the past, query traffic job was not scheduled, instead just do it now');
+    // need to do something to tell the user the query wasn't scheduled... 
+    // can send another notification to tell them that their event is in the past and provide a map to that location so they can check on their own
+    agenda.now('query traffic', travel);
+    // NOTE: THIS WILL BREAK WHEN THE USER FIRST LOGS IN, IT WILL SEND A TON OF NOTIFICATIONS
+
   }
 };
 
-const removeJob = function(id) {
-  console.log('id in removeJob for queryTraffic', id);
+const removeQueryTraffic = function(id) {
   agenda.cancel({_id: id}, (err, numRemoved) => {
     if (err) {
       console.warn('error in removing job:', err)
     } else {
-      console.log(numRemoved, 'jobs removed');
+      console.log(numRemoved, 'query traffic job removed');
     }
   });
 };
