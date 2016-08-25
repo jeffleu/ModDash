76ab842bd18bb5f94c4451bb3944b2092322be37
Carl Chen <carlchen214@gmail.com>
2016-08-24 20:35:25 -0700

    fix webpack to also compile client and web

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require('path');
 
 module.exports = [
-  // {
-  //   name: "extension",
-  //   entry: [path.resolve(__dirname, 'client', 'src', 'components', 'App.jsx'), 'whatwg-fetch'],
-  //   output: {
-  //     path: __dirname + '/client/dist',
-  //     filename: 'bundle.js'
-  //   },
-  //   // devServer: {
-  //   //   inline: true,
-  //   //   contentBase: __dirname + '/client/dist',
-  //   //   port: 8100
-  //   // },
-  //   module: {
-  //     loaders: [
-  //       {
-  //         test: /\.jsx?/,
-  //         exclude: /node_modules/,
-  //         include: /client/,
-  //         loaders: ['react-hot', 'babel']
-  //       }
-  //     ],
-  //   },
-  //   externals: {
-  //     "jquery": "jQuery",
-  //     "Chrono": "Chrono",
-  //     "artyom": "artyom",
-  //     "moment": "moment"
-  //   }    
-  // },
+  {
+    name: "extension",
+    entry: [path.resolve(__dirname, 'client', 'src', 'components', 'App.jsx'), 'whatwg-fetch'],
+    output: {
+      path: __dirname + '/client/dist',
+      filename: 'bundle.js'
+    },
+    // devServer: {
+    //   inline: true,
+    //   contentBase: __dirname + '/client/dist',
+    //   port: 8100
+    // },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          include: /client/,
+          loaders: ['react-hot', 'babel']
+        }
+      ],
+    },
+    externals: {
+      "jquery": "jQuery",
+      "Chrono": "Chrono",
+      "artyom": "artyom",
+      "moment": "moment"
+    }    
+  },
   {
     name: "web",
     entry: ['./web/index.jsx', './web/dist/styles/main.scss'],
