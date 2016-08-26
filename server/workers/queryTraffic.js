8893ef832822cffb193fa7eedf3459af4cf8c215
Derek Hoang <hoangda23@gmail.com>
2016-08-25 17:20:16 -0700

    added some comments to delete mongo db

--- server/workers/queryTraffic.js
+++ server/workers/queryTraffic.js
@@ -4,18 +4,18 @@ const getTrafficTime = require('../utility/map/getTrafficTime');
 const sendLeaveNotification = require ('./sendLeaveNotification');
 
 const recurringCheck = (event) => {
-  console.log('inside recurringCheck');  
+  console.log('inside recurringCheck');
   const timeToCheckAgain = 180000; // check every 3 min
   getTrafficTime(event)
   .then(times => {
     // 15 minutes before hand
     if (Date.now() + times.traffic + 1500000 >= Date.parse(event.dataValues.startdatetime)) {
-      console.log('traffic is over start time, sending notification'); 
+      console.log('traffic is over start time, sending notification');
       event.dataValues.traffic = times.traffic;
       sendLeaveNotification(times.notificationTime, event.dataValues);
     } else {
       console.log('traffic is not over start time, do setTimeout to make another check in 3 minutes');
-      // polling is a time-sensitive way to query data 
+      // polling is a time-sensitive way to query data
       setTimeout(function() {
         console.log('setTimeout recurringCheck triggered 3 minute later');
         recurringCheck(event);
@@ -37,7 +37,7 @@ agenda.define('query traffic', (job, done) => {
   setTimeout(() => {
     removeQueryTraffic(job.attrs._id);
   }, 1000);
-    
+
   // then agenda.cancel({id: jobiD}) or some other kind of identifier of the job we were doing
   done();
 });
@@ -50,7 +50,7 @@ const queryTraffic = travel => {
     agenda.schedule(travel.queryTime, 'query traffic', travel);
   } else {
     console.log('query time is in the past, query traffic job was not scheduled, instead just do it now');
-    // need to do something to tell the user the query wasn't scheduled... 
+    // need to do something to tell the user the query wasn't scheduled...
     // can send another notification to tell them that their event is in the past and provide a map to that location so they can check on their own
     agenda.now('query traffic', travel);
     // NOTE: THIS WILL BREAK WHEN THE USER FIRST LOGS IN, IT WILL SEND A TON OF NOTIFICATIONS
@@ -67,5 +67,7 @@ const removeQueryTraffic = function(id) {
     }
   });
 };
+
+// agenda.cancel){data: {id: travel.id}}
 
 module.exports = queryTraffic;
