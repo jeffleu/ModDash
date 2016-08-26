4399773ca621e0ddc91c23ba9d527d706a1c2e09
Jeff Leu <babybluejeff@Jeffs-MacBook-Pro.local>
2016-08-26 13:27:29 -0700

    Added circle creator pictures to splash page.

--- web/src/Creators.jsx
+++ web/src/Creators.jsx
@@ -6,17 +6,17 @@ class Creators extends React.Component {
     return (
       <Section>
         <CustomerQuotes>
-          <CustomerQuote name="Carl Chen" title="Velocity co-creator" imageUrl="./assets/carl_chen_square1.jpg">
+          <CustomerQuote name="Carl Chen" title="Velocity co-creator" imageUrl="./assets/carl_chen_circle.png">
             <p>
             We know your new browser tab is something you see all the time so we made Velocity to be sleek and minimal. Most importantly, we made it load fast and accurately.
             </p>
           </CustomerQuote>
-          <CustomerQuote name="Derek Hoang" title="Velocity co-creator" imageUrl="./assets/derek_hoang_square1.jpg">
+          <CustomerQuote name="Derek Hoang" title="Velocity co-creator" imageUrl="./assets/derek_hoang_circle.png">
             <p>
             Our app efficiently saves users time from checking their google calendar and maps for directions, traffic, and arrival time. Users will never have to worry about arriving late to an event again. We'll tell you when and where you'll need to be!
             </p>
           </CustomerQuote>
-          <CustomerQuote name="Jeff Leu" title="Velocity co-creator" imageUrl="./assets/jeff_leu_square1.jpg">
+          <CustomerQuote name="Jeff Leu" title="Velocity co-creator" imageUrl="./assets/jeff_leu_circle.png">
             <p>
             The Google Chrome browser's native APIs are so powerful, so it was very exciting to make a voice-activated dashboard that leveraged its technologies.
             </p>
